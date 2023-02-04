d909e3c524eea1b60bdbdeac0d024363b35c3b26
zainr24-1 <zain_rafiq@asl.org>
2023-02-04 14:46:18 +0000

    Arm subsystem for 2 motors, both limited by Digital Inputs.

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,44 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+
+  private static Arm instance;
+
+  public static Arm getInstance() {
+    if (instance == null)
+      instance = new Arm();
+    return instance;
+  }
+
+  CANSparkMax motor1 = new CANSparkMax(0, null);
+  CANSparkMax motor2 = new CANSparkMax(0, null);
+
+  DigitalInput topLine = new DigitalInput(0);
+  DigitalInput topLimit = new DigitalInput(0);
+  DigitalInput bottomLine = new DigitalInput(0);
+  DigitalInput bottomLimit = new DigitalInput(0);
+
+  private static void moveMotor(CANSparkMax motor, DigitalInput top, DigitalInput bottom, double speed) {
+    if ((speed < 0) && (bottom.get()))
+      motor.set(0);
+    else if (top.get())
+      motor.set(0);
+    else
+      motor.set(1);
+  }
+
+  public Command getMotor1Command(double speed) {
+    return new InstantCommand(() -> moveMotor(motor1, topLine, bottomLine, speed));
+  }
+
+  public Command getMotor2Command(double speed) {
+    return new InstantCommand(() -> moveMotor(motor2, topLimit, bottomLimit, speed));
+  }
+}
