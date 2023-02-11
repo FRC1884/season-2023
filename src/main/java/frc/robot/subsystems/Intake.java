110ba993053aee9fd657ce8464cf5e00cfaa09f5
zainr24-1 <zain_rafiq@asl.org>
2023-02-11 10:23:38 +0000

    intake subsystem

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+
+  private static Intake instance;
+
+  public static Intake getInstance() {
+    if (instance == null)
+      instance = new Intake();
+    return instance;
+  }
+
+  CANSparkMax motor = new CANSparkMax(0, null);
+
+  public Command intakeCommand(double speed) {
+    return new InstantCommand(() -> {
+      motor.set(speed);
+    });
+  }
+
+}
