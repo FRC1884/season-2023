b6362b1364a51afd53f7fae7c79560b379f90327
Nekarone <yifei_yan@asl.org>
2023-12-01 17:28:25 +0000

    changes to test only one swerve module

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -9,14 +9,14 @@ import frc.robot.util.controllers.Logitech.Version;
 public class Config {
   public class Subsystems {
     public static final boolean SWERVE_ENABLED = true;
-    public static final boolean INTAKE_MOTOR_ENABLED = true;
-    public static final boolean ELEVATOR_ARM_ENABLED = true;
+    public static final boolean INTAKE_MOTOR_ENABLED = false;
+    public static final boolean ELEVATOR_ARM_ENABLED = false;
     public static final boolean MOTION_PROFILE_ENABLED = false;
   }
 
   public class Controllers {
     public static final boolean DRIVER_ENABLED = true;
-    public static final boolean OPERATOR_ENABLED = true;
+    public static final boolean OPERATOR_ENABLED = false;
     public static final boolean TESTER_ENABLED = false;
   }
 
--- src/main/java/frc/robot/layout/DriverMap.java
+++ src/main/java/frc/robot/layout/DriverMap.java
@@ -4,7 +4,6 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Config;
 import frc.robot.subsystems.Swerve;
-import frc.robot.subsystems.Vision;
 import frc.robot.util.controllers.CommandMap;
 import frc.robot.util.controllers.GameController;
 
@@ -27,9 +26,6 @@ public abstract class DriverMap extends CommandMap {
   private void registerSwerve() {
     var swerve = Swerve.getInstance();
     swerve.setDefaultCommand(swerve.driveCommand(this::getChassisSpeeds));
-    getCubeButton().onTrue(swerve.alignWithGridCommand(() -> Vision.Position.CUBE));
-    getLeftConeButton().onTrue(swerve.alignWithGridCommand(() -> Vision.Position.LEFT_CONE));
-    getRightConeButton().onTrue(swerve.alignWithGridCommand(() -> Vision.Position.RIGHT_CONE));
   }
 
   @Override
